8925e913514fc04bd65c988c29905f63a28e132a
李光远 <1428114973@qq.com>
2023-01-11 23:00:42 +0800

    第一版提交:实现了完整了自动化功能

---
+++ bin/index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+const { Command } = require('commander')
+const program = new Command()
+const index = require('../lib/index.js')
+program
+  .version(require('../package.json').version, '-v, --version')
+  .description('获取当前工具的版本号')
+  .usage('-v, --version')
+program
+  .command('<size>')
+  .description('扩大引擎内存大小')
+  .action((size) => {
+    index(size)
+  })
